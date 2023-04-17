92faf89591986cd342db0e4c3f24eaf49963ea1a
mahanhur <124110207+mahanhur@users.noreply.github.com>
2023-04-17 15:42:09 +0900

    Update Main.java
    2nd

--- src/main/java/com/kbstar/Main.java
+++ src/main/java/com/kbstar/Main.java
@@ -3,7 +3,10 @@ package com.kbstar;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
-
+    
+        int a = 10;
+        int b = 20;
+        
         for (int i = 1; i <= 5; i++) {
 
             System.out.println("i = " + i);
