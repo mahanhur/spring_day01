d234ed857c9c8fe995bc8a7c0e36b2c89c4062db
mahanhur <allowbasmh@gmail.com>
2023-04-17 16:07:13 +0900

    2nd

--- src/main/java/com/kbstar/Main.java
+++ src/main/java/com/kbstar/Main.java
@@ -3,14 +3,15 @@ package com.kbstar;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
-    
-        int a = 10;
-        int b = 20;
         int c = 30;
 
+        Service service = new Service();
         for (int i = 1; i <= 5; i++) {
 
             System.out.println("i = " + i);
         }
     }
 }
+    
+        int a = 10;
+        int b = 20;
